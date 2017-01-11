f2dd968c74bc1125a8c560e05c3b7496141e5d3d
avelar <fortunato.avelar@gmail.com>
2017-01-11 08:40:18 -0200

    mobile first landing

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  //menu
+  $(".menu-hamburguer").on("click", function(){
+    $(".navegation-link-content").slideToggle();
+  });
+
+  //microinteractions
+
+  //scroll to top show
+  $(document).scroll(function(){
+    if ($(this).scrollTop() > 400) {
+        $('.go-top').fadeIn();
+    } else {
+        $('.go-top').fadeOut();
+    }
+  });
+
+  //scroll to top action
+  $('.go-top').each(function(){
+      $(this).click(function(){
+          $('html,body').animate({ scrollTop: 0 }, 'slow');
+          return false;
+      });
+  });
+
+  $('.icon-arrow-down').each(function(){
+      $(this).click(function(){
+          $('html,body').animate({ scrollTop: 800 }, 'slow');
+          return false;
+      });
+  });
+
+});//wrapper end
